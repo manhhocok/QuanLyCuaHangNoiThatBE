36f209034f04cb4e0640230ee219b45f5882d478
manhhocok <trjeumanh1213@gmail.com>
2024-03-10 20:13:06 +0700

    test deploy

--- src/main.ts
+++ src/main.ts
@@ -20,7 +20,7 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
-  await app.listen(3000);
+  await app.listen(process.env.POST || 3000);
   if (module.hot) {
     module.hot.accept();
     module.hot.dispose(() => app.close());
