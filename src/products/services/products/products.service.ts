4fb34fef9e42e4df5e03949d9d9d8caf9804d02c
manhhocok <trjeumanh1213@gmail.com>
2024-03-13 16:02:22 +0700

    test

--- src/products/services/products/products.service.ts
+++ src/products/services/products/products.service.ts
@@ -21,7 +21,7 @@ export class ProductsService {
       where: {
         product_id,
       },
-      relations: ['category', 'productType'],
+      relations: ['category', 'productType', 'material'],
     });
   }
 
